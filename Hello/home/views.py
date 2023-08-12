a78b9414f93975657396547ec98c2e632051af9e
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-08-13 01:02:27 +0530

    home app, url dispatching

---
+++ Hello/home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("This is home page.")
+
+def about(request):
+    return HttpResponse("This is about page.")
+
+def contact(request):
+    return HttpResponse("This is contact page.")
+
+def services(request):
+    return HttpResponse("This is services page.")
+
